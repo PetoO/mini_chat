1a421c5a203cc59ed309b78b84fb40d1fb716697
PetoO <zapalac.peter@gmail.com>
2015-08-16 09:53:06 +0200

    asd

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV['1461806970791614'], ENV['afb859b198ecd459274be401f4443c73'],
+  provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'],
            scope: 'public_profile', display: 'page', image_size: 'square'
-  provider :twitter, ENV['garpATPfyoKVp2NAtqbr2S8rl'], ENV['kkNzr4cvFNeYcyLGIjsOCIh8jt4alWl5pd2Ya7PcdaLKufEtGa'], image_size: 'normal'
+
+  provider :twitter, ENV['TWITTER_KEY'], ENV['TWITTER_SECRET'], image_size: 'normal'
 end
